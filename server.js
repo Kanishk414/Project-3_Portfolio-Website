89703db23d3485757990ed1fd7c7284b9a7a1caa
Kanishk pardikar <pardikarkanishk@gmail.com>
2024-07-17 21:40:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
